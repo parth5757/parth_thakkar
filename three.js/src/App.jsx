6034ba5a4f89d5f1abc85da32ad2fe13a3fbe9a4
parth5757 <psthakkar2@gmail.com>
2023-11-22 00:00:02 +0530

    21/11/23 last

--- three.js/src/App.jsx
+++ three.js/src/App.jsx
@@ -1,20 +1,48 @@
-import {Route, BrowseRouter as Router, Routes } from 'react-router-dom';
+// import {Route, BrowseRouter as Router, Routes } from 'react-dom/client';
+
+// const App = () => {
+//   return (
+//     <main className="bg-slate-300/20">
+//         <Router>
+//             Navbar
+//             <Routes>
+//                 <Route path= "/" element = {'Home'} />
+//                 <Route path ="/about" element = {'About'} />
+//                 <Route path ="/projects" element = {'projects'} />
+//                 <Route path ="/contacts" element = {'Contacts'} />
+//             </Routes>
+//         </Router>
+//         <h1>parth</h1>
+//     </main>
+
+//     // <div>
+//     //   <h1 className="text-3xl font-bold underline text-re">
+//     //     Hi Parth
+//     //   </h1>
+//     // </div>
+//   )
+// }
+
+// export default App
+
+
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 const App = () => {
   return (
     <main className="bg-slate-300/20">
         <Router>
-            Navbar
+            <Navbar />
             <Routes>
-                <Route path= " /" element = {'Home'}/>
-                <Route path ="/about" element = {'About'} />
-                <Route path ="/projects" element = {'projects'} />
-                <Route path ="/contacts" element = {'Contacts'} />
+                <Route path="/" element={'Home'} />
+                <Route path="/about" element={'About'} />
+                <Route path="/projects" element={'Projects'} />
+                <Route path="/contacts" element={'Contacts'} />
             </Routes>
         </Router>
-        <h1>parth</h1>
     </main>
   )
 }
 
-export default App
+export default App;
