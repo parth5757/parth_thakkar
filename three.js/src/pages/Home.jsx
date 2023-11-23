b2522bc79ce760b7b02418ced390d6da72dc8166
parth5757 <psthakkar2@gmail.com>
2023-11-23 23:39:56 +0530

    23/11/23

--- three.js/src/pages/Home.jsx
+++ three.js/src/pages/Home.jsx
@@ -11,6 +11,11 @@ const Home = () => {
       camera={{ near:0.1, far:1000 }}
       >
         <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
           
         </Suspense>
       </Canvas>
