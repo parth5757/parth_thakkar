a227effa218a35399ca591421116e09510bb0e1e
parth5757 <psthakkar2@gmail.com>
2024-02-14 05:43:12 +0530

    Navbar Update

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -11,7 +11,7 @@ function NavScrollExample() {
 return (
     <Navbar expand="lg" className="bg-body-tertiary">
     <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+        <Navbar.Brand className="nav-link" href="#">Navbar scroll</Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
         <Nav
@@ -19,11 +19,11 @@ return (
             style={{ maxHeight: '100px' }}
             navbarScroll
         >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
+            <Nav.Link className="nav-link" href="#action1">Home</Nav.Link>
+            <Nav.Link className="nav-link" href="#action2">Link</Nav.Link>
             <NavDropdown title="Link" id="navbarScrollingDropdown">
-            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action4">
+            <NavDropdown.Item className="nav-link" href="#action3">Action</NavDropdown.Item>
+            <NavDropdown.Item className="nav-link" href="#action4">
                 Another action
             </NavDropdown.Item>
             <NavDropdown.Divider />
@@ -31,7 +31,7 @@ return (
                 Something else here
             </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#" disabled>
+            <Nav.Link className="nav-link" href="#" disabled>
             Link
             </Nav.Link>
         </Nav>
